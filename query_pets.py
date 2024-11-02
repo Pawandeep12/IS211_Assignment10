506eaf81d05842340a8190ed10d7814d05f5ca03
Pawandeep12 <80072945+Pawandeep12@users.noreply.github.com>
2024-11-02 19:17:37 -0400

    Update query_pets.py

--- query_pets.py
+++ query_pets.py
@@ -1,35 +1,41 @@
 import sqlite3
 
-def query_person_pets(person_id):
+def query_pets():
     connection = sqlite3.connect('pets.db')
     cursor = connection.cursor()
 
-    # Query person data
-    cursor.execute("SELECT first_name, last_name, age FROM person WHERE id=?", (person_id,))
-    person = cursor.fetchone()
-
-    if person:
-        print(f"{person[0]} {person[1]}, {person[2]} years old")
-
-        # Query pets for that person
-        cursor.execute("""
-            SELECT pet.name, pet.breed, pet.age 
-            FROM pet
-            JOIN person_pet ON pet.id = person_pet.pet_id
-            WHERE person_pet.person_id = ?
-        """, (person_id,))
+    while True:
+        person_id = input("Enter person ID (-1 to exit): ")
         
-        pets = cursor.fetchall()
-        for pet in pets:
-            print(f"{person[0]} owned {pet[0]}, a {pet[1]}, that was {pet[2]} years old.")
-    else:
-        print("Person not found.")
+        if person_id == '-1':
+            print("Exiting.")
+            break
+
+        cursor.execute("SELECT first_name, last_name, age FROM person WHERE id = ?", (person_id,))
+        person = cursor.fetchone()
+
+        if person:
+            first_name, last_name, age = person
+            print(f"{first_name} {last_name}, {age} years old")
+
+            cursor.execute("""
+                SELECT pet.name, pet.breed, pet.age, pet.dead
+                FROM pet
+                JOIN person_pet ON pet.id = person_pet.pet_id
+                WHERE person_pet.person_id = ?
+            """, (person_id,))
+            pets = cursor.fetchall()
+
+            if pets:
+                for name, breed, age, dead in pets:
+                    status = "deceased" if dead == 1 else "alive"
+                    print(f"  - {name}, a {breed}, {age} years old, is {status}.")
+            else:
+                print("  No pets found.")
+        else:
+            print("Person not found.")
 
     connection.close()
 
 if __name__ == "__main__":
-    while True:
-        person_id = int(input("Enter a person's ID (or -1 to exit): "))
-        if person_id == -1:
-            break
-        query_person_pets(person_id)
+    query_pets()
